f3b6ba17cd6d1e63e099477dc286b9ef2c7d2e50
ly10228 <601520157@qq.com>
2018-11-06 23:00:08 +0800

    通过反射获取私有构造方法并使用 private Person(String name){}

--- src/mian/java/com/ly/reflect/demo/ConstructorDemo.java
+++ src/mian/java/com/ly/reflect/demo/ConstructorDemo.java
@@ -28,6 +28,10 @@ public class ConstructorDemo {
         //4.2 带参构造方法
         Person pWithParam = (Person) getSingleConstructorWithParam(c);
         pWithParam.show();
+        System.out.println("=================================");
+        //4.3 ：通过反射获取私有构造方法并使用
+        Person pPrivate = (Person) getPrivateSingleConstructorWithParam(c);
+        pPrivate.show();
     }
 
     /**
@@ -90,6 +94,32 @@ public class ConstructorDemo {
          * 使用此 Constructor 对象表示的构造方法来创建该构造方法的声明类的新实例，并用指定的初始化参数初始化该实例。
          */
         Object obj = con.newInstance("众小安", 5, "上海市黄浦区外滩");
+        System.out.println(obj);
+        return obj;
+    }
+
+
+    /**
+     * 通过反射获取私有带参构造函数并使用
+     * private Person(String name){}
+     * public Constructor<T> getConstructor(Class<?>... parameterTypes)
+     * parameterTypes表示：
+     * 你要获取的构造方法的构造参数个数及数据类型的class字节码文件对象
+     *
+     * @param c Person类的字节码文件
+     * @return
+     */
+    private static Object getPrivateSingleConstructorWithParam(Class c) throws Exception {
+        Constructor con = c.getDeclaredConstructor(String.class);// 返回的是构造方法对象
+        /**
+         * public T newInstance(Object... initargs)
+         * 使用此 Constructor 对象表示的构造方法来创建该构造方法的声明类的新实例，并用指定的初始化参数初始化该实例。
+         */
+        con.setAccessible(true);
+        // 用该私有构造方法创建对象
+        // IllegalAccessException:非法的访问异常。
+        // 暴力访问
+        Object obj = con.newInstance("众小安");// 值为true则指示反射的对象在使用时应该取消Java语言访问检查。
         System.out.println(obj);
         return obj;
     }
