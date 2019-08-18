d60270b9e4e03e6bf184a681db8d670b8d95a66d
ly10228 <601520157@qq.com>
2019-08-18 10:44:32 +0800

    lombda练习

--- src/main/java/com/ly/java8/lambda/exer/LongHandler.java
+++ src/main/java/com/ly/java8/lambda/exer/LongHandler.java
@@ -6,6 +6,7 @@ package com.ly.java8.lambda.exer;
  * @create 2019-08-18 10:34
  * @last modify by [LuoYong 2019-08-18 10:34]
  **/
+@FunctionalInterface
 public interface LongHandler<T, R> {
     R handler(T tOne, T tTwo);
 }
