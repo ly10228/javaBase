be80119c07329a14e2681d5a386474910617c574
ly10228 <601520157@qq.com>
2019-09-09 00:11:46 +0800

    设计模式--工厂模式--简单工厂模式--创建pizza

---
+++ src/main/java/com/ly/senior/designpatterns/creational/factory/simple/SimpleFactory.java
@@ -0,0 +1,64 @@
+package com.ly.senior.designpatterns.creational.factory.simple;
+
+import com.ly.senior.designpatterns.creational.factory.tradition.pizza.CheesePizza;
+import com.ly.senior.designpatterns.creational.factory.tradition.pizza.GreekPizza;
+import com.ly.senior.designpatterns.creational.factory.tradition.pizza.PepperPizza;
+import com.ly.senior.designpatterns.creational.factory.tradition.pizza.Pizza;
+
+/**
+ * @author luoyong
+ * @Description: SimpleFactory 简单工厂模式也成静态工厂模式
+ * @create 2019-09-08 23:43
+ * @last modify by [LuoYong 2019-09-08 23:43]
+ **/
+public class SimpleFactory {
+    /**
+     * @param orderType Pizza类型
+     * @return
+     * @Description: 根据Pizza类型创建Pizza
+     * @author luoyong
+     * @create 23:44 2019/9/8
+     * @last modify by [LuoYong 23:44 2019/9/8 ]
+     */
+    public Pizza createPizza(String orderType) {
+        Pizza pizza = null;
+        System.out.println("使用简单工厂模式");
+        if (orderType.equals("greek")) {
+            pizza = new GreekPizza();
+            pizza.setName(" 希腊披萨 ");
+        } else if (orderType.equals("cheese")) {
+            pizza = new CheesePizza();
+            pizza.setName(" 奶酪披萨 ");
+        } else if (orderType.equals("pepper")) {
+            pizza = new PepperPizza();
+            pizza.setName("胡椒披萨");
+        }
+
+        return pizza;
+    }
+
+    /**
+     * @param orderType
+     * @return
+     * @Description: 简单工厂模式 也叫 静态工厂模式
+     * @author luoyong
+     * @create 0:05 2019/9/9
+     * @last modify by [LuoYong 0:05 2019/9/9 ]
+     */
+    public static Pizza createStaticPizza(String orderType) {
+        Pizza pizza = null;
+        System.out.println("使用简单工厂模式");
+        if (orderType.equals("greek")) {
+            pizza = new GreekPizza();
+            pizza.setName(" 希腊披萨 ");
+        } else if (orderType.equals("cheese")) {
+            pizza = new CheesePizza();
+            pizza.setName(" 奶酪披萨 ");
+        } else if (orderType.equals("pepper")) {
+            pizza = new PepperPizza();
+            pizza.setName("胡椒披萨");
+        }
+
+        return pizza;
+    }
+}
---
+++ src/main/java/com/ly/senior/designpatterns/creational/factory/simple/order/OrderPizzaByStatic.java
@@ -0,0 +1,52 @@
+package com.ly.senior.designpatterns.creational.factory.simple.order;
+
+import com.ly.senior.designpatterns.creational.factory.simple.SimpleFactory;
+import com.ly.senior.designpatterns.creational.factory.tradition.pizza.Pizza;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author luoyong
+ * @Description: OrderPizza
+ * @create 2019-09-08 23:52
+ * @last modify by [LuoYong 2019-09-08 23:52]
+ **/
+public class OrderPizzaByStatic {
+
+    Pizza pizza = null;
+
+    public OrderPizzaByStatic() {
+        //用户输入的
+        String orderType = "";
+
+        do {
+            orderType = getType();
+            pizza = SimpleFactory.createStaticPizza(orderType);
+            //输出pizza
+            if (pizza != null) {
+                //订购成功
+                pizza.prepare();
+                pizza.bake();
+                pizza.cut();
+                pizza.box();
+            } else {
+                System.out.println(" 订购披萨失败 ");
+                break;
+            }
+        } while (true);
+    }
+
+    private String getType() {
+        try {
+            BufferedReader strin = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("input pizza 种类:");
+            String str = strin.readLine();
+            return str;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
