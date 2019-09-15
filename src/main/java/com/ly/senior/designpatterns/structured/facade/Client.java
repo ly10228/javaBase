56e8f8dffb7b22f2173f46a0b72a694682663ab0
ly10228 <601520157@qq.com>
2019-09-15 16:31:21 +0800

    设计模式--外观设计模式--场景

--- src/main/java/com/ly/senior/designpatterns/structured/facade/Client.java
+++ src/main/java/com/ly/senior/designpatterns/structured/facade/Client.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 /**
  * @author luoyong
  * @Description: Client
+ * 场景
+ * 组建一个家庭影院：DVD播放器、投影仪、自动屏幕、环绕立体声、爆米花机,要求完成使用家庭影院的功能，其过程为：• 直接用遥控器：统筹各设备开关• 开爆米花机• 放下屏幕• 开投影仪• 开音响• 开DVD，选dvd• 去拿爆米花• 调暗灯光• 播放• 观影结束后，关闭各种设备
  * @create 2019-09-15 16:22
  * @last modify by [LuoYong 2019-09-15 16:22]
  **/
