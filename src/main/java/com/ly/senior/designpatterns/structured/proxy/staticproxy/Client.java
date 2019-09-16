aa3256a7a42482320e4bb12e48df04b22d48a73b
ly10228 <601520157@qq.com>
2019-09-16 22:23:16 +0800

    设计模式--代理模式

--- src/main/java/com/ly/senior/designpatterns/structured/proxy/staticproxy/Client.java
+++ src/main/java/com/ly/senior/designpatterns/structured/proxy/staticproxy/Client.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 /**
  * @author luoyong
  * @Description: Client
+ * 优点：在不修改目标对象的功能前提下面，能通过代理对象对目标功能进行扩展
+ * 缺点：
+ *   因为代理对象需要与被代理对象实现一样的接口，所有会有很多代理类
+ *   一旦接口增加方法，目标对象与代理对象都要维护
  * @create 2019-09-15 22:27
  * @last modify by [LuoYong 2019-09-15 22:27]
  **/
