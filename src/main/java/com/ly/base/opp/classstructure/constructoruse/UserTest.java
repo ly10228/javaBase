36bec70fda15410eda4629517023159df7f83a4f
ly10228 <601520157@qq.com>
2019-10-13 17:54:04 +0800

    java基础--面向对象--构造器

---
+++ src/main/java/com/ly/base/opp/classstructure/constructoruse/UserTest.java
@@ -0,0 +1,50 @@
+package com.ly.base.opp.classstructure.constructoruse;
+
+import org.junit.Test;
+
+/**
+ * @author luoyong
+ * @Description: 总结：属性赋值的先后顺序
+ * ① 默认初始化
+ * ② 显式初始化
+ * ③ 构造器中初始化
+ * ④ 通过"对象.方法" 或 "对象.属性"的方式，赋值
+ * <p>
+ * 以上操作的先后顺序：① - ② - ③ - ④
+ * @create 2019-10-13 17:37
+ * @last modify by [LuoYong 2019-10-13 17:37]
+ **/
+public class UserTest {
+
+    @Test
+    public void test() {
+        User u = new User();
+
+        System.out.println(u.age);
+
+        User u1 = new User(2);
+
+        u1.setAge(3);
+        u1.setAge(5);
+
+        System.out.println(u1.age);
+    }
+}
+
+class User {
+    String name;
+    int age = 1;
+
+    public User() {
+
+    }
+
+    public User(int a) {
+        age = a;
+    }
+
+    public void setAge(int a) {
+        age = a;
+    }
+
+}
