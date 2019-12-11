d0ee33f0f23c8938206b323c543b9543c94f84ab
luoyong001 <luoyong001@zhongan.com>
2019-12-11 20:23:12 +0800

    迭代器注解

--- src/main/java/com/ly/base/collection/IteratorTest.java
+++ src/main/java/com/ly/base/collection/IteratorTest.java
@@ -11,6 +11,11 @@ import java.util.Iterator;
 /**
  * @author luoyong
  * @Description: IteratorTest
+ * 集合元素的遍历操作，使用迭代器Iterator接口
+ * 1.内部的方法：hasNext() 和  next()
+ * 2.集合对象每次调用iterator()方法都得到一个全新的迭代器对象，
+ * 默认游标都在集合的第一个元素之前。
+ * 3.内部定义了remove(),可以在遍历的时候，删除集合中的元素。此方法不同于集合直接调用remove()
  * @create 2019-12-10 22:53
  * @last modify by [LuoYong 2019-12-10 22:53]
  **/
