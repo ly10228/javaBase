727fdb537ed1a0ae5f453aa08bf9e38025bea9cf
luoyong002 <luoyong002@zhongan.com>
2020-07-10 17:51:26 +0800

    图-深度优先和广度优先的差别

--- src/main/java/com/ly/senior/datastructure/graph/GraphTest.java
+++ src/main/java/com/ly/senior/datastructure/graph/GraphTest.java
@@ -41,6 +41,53 @@ public class GraphTest {
     }
 
     /**
+     * @return void
+     * @Description: 测试深度优先遍历和广度优先遍历的差别
+     * @author luoyong
+     * @create 5:15 下午 2020/7/10
+     * @last modify by [LuoYong 5:15 下午 2020/7/10 ]
+     */
+    @Test
+    public void testDfsBfsDiff() {
+        String vertexs[] = {"1", "2", "3", "4", "5", "6", "7", "8"};
+
+        //1:创建图对象
+        Graph graph = new Graph(vertexs.length);
+
+        //2:循环添加顶点
+        for (String vertex : vertexs) {
+            graph.insertVertex(vertex);
+        }
+
+        //3:添加边
+        graph.insertEdge(0, 1, 1);
+        graph.insertEdge(0, 2, 1);
+        graph.insertEdge(1, 3, 1);
+        graph.insertEdge(1, 4, 1);
+        graph.insertEdge(3, 7, 1);
+        graph.insertEdge(4, 7, 1);
+        graph.insertEdge(2, 5, 1);
+        graph.insertEdge(2, 6, 1);
+        graph.insertEdge(5, 6, 1);
+
+
+        //显示一把邻结矩阵
+        graph.showGraph();
+
+        //测试一把，我们的dfs遍历是否ok
+        System.out.println("深度遍历");
+        // A->B->C->D->E [1->2->4->8->5->3->6->7]
+        graph.dfs();
+        System.out.println();
+        System.out.println("广度优先!");
+        // A->B->C->D-E [1->2->3->4->5->6->7->8]
+        graph.bfs();
+
+
+    }
+
+
+    /**
      * @param
      * @return void
      * @Description: 测试广度优先遍历
