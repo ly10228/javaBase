ec6fb8462541afd0a05662f65b6d2ee45a8f1b92
ly10228 <601520157@qq.com>
2021-03-06 17:49:23 +0800

    jps示例

---
+++ src/main/java/com/ly/base/jvm/monitor/commandline/jps/ScannerTest.java
@@ -0,0 +1,16 @@
+package com.ly.base.jvm.monitor.commandline.jps;
+
+import java.util.Scanner;
+
+/**
+ * @author luoyong
+ *  * @create 2021-03-06 3:37 下午
+ *  * @last modify by [luoyong 2021-03-06 3:37 下午]
+ * @Description: ScannerTest
+ **/
+public class ScannerTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String info = scanner.next();
+    }
+}
