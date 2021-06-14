169980baa4d38c89a912ec95a67f7dab53dcca52
ly10228 <601520157@qq.com>
2021-06-14 16:32:07 +0800

    建造者模式

---
+++ src/main/java/com/ly/senior/designpatterns/sgg/creatation/builder/AbstractBuilder.java
@@ -0,0 +1,23 @@
+package com.ly.senior.designpatterns.sgg.creatation.builder;
+
+/**
+ * 抽象建造者
+ */
+public abstract class AbstractBuilder {
+
+    Phone phone;
+
+    abstract AbstractBuilder customCpu(String cpu);
+
+    abstract AbstractBuilder customMem(String mem);
+
+    abstract AbstractBuilder customDisk(String disk);
+
+    abstract AbstractBuilder customCam(String cam);
+
+    Phone getProduct() {
+        return phone;
+    }
+
+
+}
