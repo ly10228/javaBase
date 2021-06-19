2bb026e569a50f2511a5e90aaa86a7e25dc4836c
ly10228 <601520157@qq.com>
2021-06-19 16:19:55 +0800

    volatile保证的变量的内存可见性

--- src/main/java/com/ly/senior/juc/jmm/VolatileSeeDemo.java
+++ src/main/java/com/ly/senior/juc/jmm/VolatileSeeDemo.java
@@ -9,16 +9,59 @@ import java.util.concurrent.TimeUnit;
  * @Description: VolatileSeeDemo
  **/
 public class VolatileSeeDemo {
-    //不加volatile，没有可见性
-    //static          boolean flag = true;
+
+    public static void main(String[] args) {
+        m1();
+//        m2();
+    }
+
     //加了volatile，保证可见性
     static volatile boolean flag = true;
 
-    public static void main(String[] args) {
+    /**
+     * @return void
+     * @Description: flag修改之后 会立即刷新回主存 同时其他线程本地的这个flag变量失效 重新从主存当中获取
+     * @author luoyong
+     * @create 3:56 下午 2021/6/19
+     * @last modify by [LuoYong 3:56 下午 2021/6/19 ]
+     */
+    public static void m1() {
         new Thread(() -> {
             System.out.println(Thread.currentThread().getName() + "\t" + "---come in");
             while (flag) {
                 new Integer(308);
+                System.out.println("flag===" + flag);
+            }
+            System.out.println("t1 over");
+        }, "t1").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(() -> {
+            flag = false;
+        }, "t2").start();
+    }
+
+    //不加volatile，没有可见性
+    static boolean commonFlag = true;
+
+    /**
+     * @return void
+     * @Description: commonFlag 没有内存可见性 线程t1会一直运行下去
+     * @author luoyong
+     * @create 3:54 下午 2021/6/19
+     * @last modify by [LuoYong 3:54 下午 2021/6/19 ]
+     */
+    public static void m2() {
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "\t" + "---come in");
+            while (commonFlag) {
+                new Integer(308);
+                System.out.println("commonFlag===" + commonFlag);
             }
             System.out.println("t1 over");
         }, "t1").start();
